c4c9406eef380057680e702c4889f4d69a9cd973
Sooseki <bonnetmanon3006@gmail.com>
2022-07-16 21:02:28 +0200

    messages components

---
+++ src/Components/Messages/Messages.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Messages = () => {
+  return (
+    <div className="Messages">
+      <ul>
+      </ul>
+    </div>
+  );
+}
+
+export default Messages;
