ce51083b127bc5f19d4418b3db84bcb369fdfc57
Sooseki <bonnetmanon3006@gmail.com>
2022-07-19 00:07:53 +0200

    routes update

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
+import { user } from '../Api/interfaces';
 import Header from '../Components/Header/Header';
+import AllTickets from '../Templates/AllTickets/AllTickets';
 import Chat from '../Templates/Chat/Chat';
 import ChatForm from '../Templates/ChatForm/ChatForm';
 import Login from '../Templates/Login/Login';
 import Register from '../Templates/Register/Register';
 import './App.scss';
 
+
 const App = () => {
+
+  const [user, setUser] = useState<user>()
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem('user')
+    console.log(userInfo)
+    if (userInfo) {
+      setUser(JSON.parse(userInfo))
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Header />
+      <Header user={user} />
       <Routes>
         <Route path="/" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/discussion">
-          <Route path="start" element={<ChatForm />}></Route>
-          <Route path=":id" element={<Chat />}></Route>
-        </Route>
+        {user &&
+          <Route>
+            <Route path="/discussion">
+              <Route path="start" element={<ChatForm user={user} />}></Route>
+              <Route path="chat" element={<Chat user={user} />}></Route>
+            </Route>
+            <Route path="/tickets">
+              <Route path="all"  element={<AllTickets user={user} />} />
+            </Route>
+          </Route>
+        }
       </Routes>
     </div>
   );
